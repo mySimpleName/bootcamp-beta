a9d8b92a9a3dbc5a8737779444c49ba9d594d277
Aleksandr <samorodnikov@gmail.com>
2023-03-25 12:48:41 +0300

    commit1: create variables

---
+++ src/main/java/ru/faang/school/task_1/Creature.java
@@ -0,0 +1,10 @@
+package ru.faang.school.task_1;
+
+public abstract class Creature {
+    private static String name;
+    private static int level;
+    private static int damage;
+    private static int defense;
+    private static int speed;
+    private static int quantity;
+}
